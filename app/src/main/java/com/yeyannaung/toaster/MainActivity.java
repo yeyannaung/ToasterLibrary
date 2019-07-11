69596494bc05a0101c196f465ed068774f63d77e
Ye Yan Naung <yeyannaung.bit@gmail.com>
2019-07-11 15:16:54 +0630

    initial commit

---
+++ app/src/main/java/com/yeyannaung/toaster/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yeyannaung.toaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
